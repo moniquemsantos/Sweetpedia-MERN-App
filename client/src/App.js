a5f8130efa60129476de4cb335c1768c289c8a6b
Monique Santos <moniquemattosm@gmail.com>
2023-02-15 13:50:12 +0100

    installing react-router and creating routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import { useEffect } from "react";
 import { Container } from "@mui/material";
 import { ThemeProvider } from "@mui/system";
 import theme from "./styles/theme";
 import HomePage from "./views/Home";
+import SignUp from "./views/SignUp";
+import Login from "./views/Login";
+import UserProfile from "./views/UserProfile";
+
 
 function App() {
   useEffect(() => {
@@ -18,7 +23,12 @@ function App() {
           background: "#fff",
         }}
       >
-        <HomePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="userprofile" element={<UserProfile />} />
+        </Routes>
       </Container>
     </ThemeProvider>
   );
