80e780a98052afc5350a199d96780d52b134452f
Monique Santos <monique@blackmagicbox.com.br>
2023-03-06 23:40:29 +0100

    Improve Code Styling

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -7,21 +7,27 @@ function Details({ recipe, formattedIngredients = "" }) {
       <Typography variant="h2" gutterBottom>
         {recipe?.title}
       </Typography>
+
       <img
         src={recipe?.image}
         alt="dessert_img"
         style={{ width: "600px", height: "700px" }}
-      ></img>
+      />
+
       <Typography variant="h5" gutterBottom>
         Ingredients
       </Typography>
+
       {formattedIngredients?.map((line) => (
         <Typography>{line}</Typography>
       ))}
+
       <Divider variant="middle" style={{ margin: "20px 0 10px 0px" }} />
+      
       <Typography variant="h5" gutterBottom>
         Instructions
       </Typography>
+
       {recipe?.instructions}
       <Divider variant="middle" style={{ margin: "20px 0 10px 0px" }} />
     </MainPaper>
